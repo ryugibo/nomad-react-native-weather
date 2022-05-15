a75e6f73f7fc41280d03564fa57795672af44afe
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-15 22:45:38 +0900

    :pencil2: 온도 표시 조정

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import { useEffect, useState } from "react";
 import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
@@ -49,28 +50,29 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>{city}</Text>
       </View>
-      <ScrollView
-        pagingEnabled
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.weather}
-      >
+      <View style={styles.weather}>
         {days.length === 0 ? (
-          <View style={styles.day}>
-            <ActivityIndicator color="white" size="large" />
-          </View>
+          <ActivityIndicator color="white" size="large" />
         ) : (
-          days.map((day, index) => (
-            <View key={index} style={styles.day}>
-              <Text style={styles.temp}>
-                {parseFloat(day.temp.day).toFixed(1)}
-              </Text>
-              <Text style={styles.description}>{day.weather[0].main}</Text>
-              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
-            </View>
-          ))
+          <ScrollView
+            pagingEnabled
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            {days.map((day, index) => (
+              <View key={index} style={styles.day}>
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Text style={styles.description}>{day.weather[0].main}</Text>
+                <Text style={styles.tinyText}>
+                  {day.weather[0].description}
+                </Text>
+              </View>
+            ))}
+          </ScrollView>
         )}
-      </ScrollView>
+      </View>
     </View>
   );
 }
@@ -89,14 +91,14 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: "500",
   },
-  weather: {},
+  weather: { flex: 2.5, alignItems: "center", justifyContent: "center" },
   day: {
     width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 158,
   },
   description: {
     marginTop: -30,
