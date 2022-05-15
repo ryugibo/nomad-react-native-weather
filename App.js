7ec1abb4ed6273b3f6fffeb2bcc2ba106071cee8
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-15 23:08:10 +0900

    :memo: #2.10 | Icons

--- App.js
+++ App.js
@@ -14,6 +14,15 @@ const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const API_KEY = "b9f77474d3fc53be4b542a6cab77b8fa";
 
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 export default function App() {
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
@@ -39,6 +48,7 @@ export default function App() {
       `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric&lang=kr`
     );
     const json = await response.json();
+    console.log(json.daily);
     setDays(json.daily);
   };
 
@@ -54,16 +64,29 @@ export default function App() {
         {days.length === 0 ? (
           <ActivityIndicator color="white" size="large" />
         ) : (
-          <ScrollView
-            pagingEnabled
-            horizontal
-            showsHorizontalScrollIndicator={false}
-          >
+          <ScrollView pagingEnabled horizontal>
             {days.map((day, index) => (
               <View key={index} style={styles.day}>
-                <Text style={styles.temp}>
-                  {parseFloat(day.temp.day).toFixed(1)}
+                <Text style={styles.dateText}>
+                  {new Date(day.dt * 1000).toString().substring(0, 10)}
                 </Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    width: "100%",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Text style={styles.temp}>
+                    {parseFloat(day.temp.day).toFixed(1)}
+                  </Text>
+                  <Fontisto
+                    name={icons[day.weather[0].main]}
+                    size={68}
+                    color="black"
+                  />
+                </View>
                 <Text style={styles.description}>{day.weather[0].main}</Text>
                 <Text style={styles.tinyText}>
                   {day.weather[0].description}
@@ -83,7 +106,7 @@ const styles = StyleSheet.create({
     backgroundColor: "tomato",
   },
   city: {
-    flex: 1.2,
+    flex: 1.5,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -91,14 +114,17 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: "500",
   },
-  weather: { flex: 2.5, alignItems: "center", justifyContent: "center" },
+  weather: { flex: 2, alignItems: "center", justifyContent: "center" },
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
+  },
+  dateText: {
+    fontSize: 60,
   },
   temp: {
-    marginTop: 50,
-    fontSize: 158,
+    fontSize: 118,
   },
   description: {
     marginTop: -30,
